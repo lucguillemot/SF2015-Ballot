31589f92f5520d07b9cc1d95399a50db95895956
lucguillemot <luc.guillemot@gmail.com>
2015-11-23 16:19:42 -0800

    added a diverging color scale

--- data/DataProcessing.R
+++ data/DataProcessing.R
@@ -26,7 +26,13 @@ propSummary <- function(prop, letter) {
       prop %>% group_by(PrecinctID)%>% summarise(Yes=sum(Yes),
                                                    No=sum(No),
                                                    Ballots=sum(Ballots.Cast),
-                                                   Turnout=sum(Turnout....))%>% mutate(prop=letter, PYes=Yes/(Yes+No), PNo=No/(Yes+No)) %>% mutate(Perc=ifelse(PYes>PNo,PYes,-(PNo)))}
+                                                   Turnout=sum(Turnout....))%>% mutate(prop=letter, PYes=Yes/(Yes+No), PNo=No/(Yes+No)) %>% mutate(Perc=ifelse(PYes>PNo,PYes,PNo)) %>% mutate(Winner=ifelse(PYes>PNo,"Yes", "No"))}
+
+# Function to remove two precincts with inconsistent values
+removePrecinct <- function(prop) {
+      prop<-prop[!prop$PrecinctID==7648,]
+      prop<-prop[!prop$PrecinctID==7933,]
+}
 
 propA <- propSummary(propA, "A")
 propB <- propSummary(propB, "B")
@@ -40,13 +46,33 @@ propI <- propSummary(propI, "I")
 propJ <- propSummary(propJ, "J")
 propK <- propSummary(propK, "K")
 
-
+propA <- removePrecinct(propA)
+propB <- removePrecinct(propB)
+propC <- removePrecinct(propC)
+propD <- removePrecinct(propD)
+propE <- removePrecinct(propE)
+propF <- removePrecinct(propF)
+propG <- removePrecinct(propG)
+propH <- removePrecinct(propH)
+propI <- removePrecinct(propI)
+propJ <- removePrecinct(propJ)
+propK <- removePrecinct(propK)
 # propI4 <- propI %>% mutate(Perc=ifelse(PYes>PNo,PYes,-(PNo)))
 
 # Create a big dataframe with all the propositions
 # prop<-rbind(propA,propB,propC,propD,propE,propF,propG,propH,propI,propJ,propK)
 
 
-#write.csv(propI, file="data/propI.csv")
+write.csv(propA, file="data/propA.csv")
+write.csv(propB, file="data/propB.csv")
+write.csv(propC, file="data/propC.csv")
+write.csv(propD, file="data/propD.csv")
+write.csv(propE, file="data/propE.csv")
+write.csv(propF, file="data/propF.csv")
+write.csv(propG, file="data/propG.csv")
+write.csv(propH, file="data/propH.csv")
+write.csv(propI, file="data/propI.csv")
+write.csv(propJ, file="data/propJ.csv")
+write.csv(propK, file="data/propK.csv")
 
 
